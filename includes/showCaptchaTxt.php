ba5f90d39e32a6b77cd56453a96604372a06b82b
Klaus <drklaus@bpmsg.org>
2022-02-08 11:44:01 +0800

    rs.css, showCaptchaTxt.php added

---
+++ includes/showCaptchaTxt.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Generates text captcha challange using api of
+ * https://textcaptcha.com/
+ * 
+ */
+
+$version = substr('$LastChangedDate: 2022-02-06 09:58:40 +0800 (Sun, 06 Feb 2022) $',18,10);
+$rev = trim('$Rev: 106 $', "$");
+
+$cptUrl = 'http://api.textcaptcha.com/bpmsg.com.json';
+$cpt = json_decode(file_get_contents($cptUrl),true);
+
+// --- TODO: extend fallback challenge
+if (!$cpt) {
+ $cpt = array( // fallback challenge
+  'q'=>"Is ice hot or cold?",
+  'a'=>array(md5("cold"))
+ );
+}
+
+$_SESSION['captcha'] = $cpt['a'];
+echo htmlentities($cpt['q']);
